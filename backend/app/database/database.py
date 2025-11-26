375fa083d4c9a8b4f38fbf6011868a40add4effc
Sebcan96 <sebastian.candia96@gmail.com>
2025-11-26 13:18:06 +0100

    la till sql.alchemy.exc för enklare identifiera fel ist krasch

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError  # <--- NY RAD
 
 #fixa .env fil för att inte lägga in lösenord och användarnamn i koden så vi ej pushar det till github
 #fixa även logiken med database_url
-DATABASE_URL = "postgresql://användarnamn:lösenord@localhost:5432/databasnamn"
+DATABASE_URL = "postgresql://postgres:lösenord@localhost:5432/databasnamn"
 
 
-engine = create_engine(DATABASE_URL, echo=True)#echo=True loggar alla sql frågor till konsolen
+engine = create_engine(DATABASE_URL, echo=True)
 
 """
 skapar session-fabrik för databassessioner
@@ -18,6 +19,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 """basklass för deklarativa modeller
    alla databasmodeller bör ärva från denna Base"""
 Base = declarative_base()
+
+
 
 
 def get_db():
