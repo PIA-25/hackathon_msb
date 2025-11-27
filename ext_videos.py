da227824fab1b2d1e95259460c8697f30726a292
Jägermeist <philipthorell@outlook.com>
2025-11-27 16:48:18 +0100

    added scene extension base code

---
+++ ext_videos.py
@@ -0,0 +1,33 @@
+import time
+from google import genai
+from google.genai import types
+from google.genai.types import GenerateVideosOperation
+
+client = genai.Client()
+
+
+operation: GenerateVideosOperation = None
+
+prompt = "Track the butterfly into the garden as it lands on an orange origami flower. A fluffy white puppy runs up and gently pats the flower."
+
+operation = client.models.generate_videos(
+    model="veo-3.1-generate-preview",
+    video=operation.response.generated_videos[0].video, # This must be a video from a previous generation
+    prompt=prompt,
+    config=types.GenerateVideosConfig(
+        number_of_videos=1,
+        resolution="720p"
+    ),
+)
+
+# Poll the operation status until the video is ready.
+while not operation.done:
+    print("Waiting for video generation to complete...")
+    time.sleep(10)
+    operation = client.operations.get(operation)
+
+# Download the video.
+video = operation.response.generated_videos[0]
+client.files.download(file=video.video)
+video.video.save("veo3.1_extension.mp4")
+print("Generated video saved to veo3.1_extension.mp4")
